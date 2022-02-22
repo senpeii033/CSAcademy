ff693393b7a52bcd87823a4c343eef209f2711f9
Brunner Adam J <BrunnerAdamJ@JohnDeere.com>
2022-02-22 14:11:04 -0600

    feat: call echo api with message

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -3,10 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,35 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.setText(message);
+
+        Downloader downloader = new Downloader();
+        downloader.execute(message);
+    }
+
+    private class Downloader extends AsyncTask<String, String, String> {
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+            Request request = new Request.Builder()
+                    .url("https://postman-echo.com/get?message=" + params[0])
+                    .build();
+
+            String string = "";
+            try (Response response = client.newCall(request).execute()) {
+                string = response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return string;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(s);
+        }
     }
 }
